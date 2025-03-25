dff1377d5063340fb428ceacc503c48cf2c2e223
leejongwon <ljw8373@gmail.com>
2025-03-25 15:00:56 +0900

    add stream

--- common/llm/openai.py
+++ common/llm/openai.py
@@ -1,23 +1,27 @@
 from openai import OpenAI
 import enum
-
+import time
 class OPENAI_LLMs(enum.Enum):
-  gpt_4o_mini = (enum.auto(), "gpt-4o-mini") 
-  gpt_4o = (enum.auto(), "gpt-4o") 
+    gpt_4o_mini = (enum.auto(), "gpt-4o-mini") 
+    gpt_4o = (enum.auto(), "gpt-4o") 
 
 
 def get_llm():
-  return OpenAI()
+    return OpenAI()
 
 def get_response_from_llm(llm, messages, llm_name:str):
-  if llm_name not in OPENAI_LLMs.__members__:
-    raise Exception("허락한 OpenAI 모델명이 아닙니다.") 
+    if llm_name not in OPENAI_LLMs.__members__:
+        raise Exception("허락한 OpenAI 모델명이 아닙니다.") 
 
-  # 전체 대화 내역을 OpenAI에 전달
-  response = llm.chat.completions.create(
-      model=OPENAI_LLMs[llm_name].value[1], # "gpt-4o-mini"
-      messages=messages
-  )
+    # 전체 대화 내역을 OpenAI에 전달
+    response = llm.chat.completions.create(
+        model=OPENAI_LLMs[llm_name].value[1], # "gpt-4o-mini"
+        messages=messages,
+        stream=True
+    )
 
-  return response.choices[0].message.content
+    for token in response:
+        if token.choices[0].delta.content is not None:
+            yield token.choices[0].delta.content
+            time.sleep(0.05)
 
